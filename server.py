8aa62916d7b61d536ad7bdacb1a8b4cf2c669932
Nuredin <103601123+nunu2729@users.noreply.github.com>
2022-12-13 21:27:11 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+
+host = input("host:")
+port = int(input("port:"))
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen(6)
+
+while True:
+    
+    communication_socket, address=server.accept()
+    print(f"Connected to {address}")
+    message = communication_socket.recv(1024).decode('utf-8')
+    print(f"Message from client is: {message}")
+    communication_socket.send(f"Got your message! thank you!".encode('utf-8'))
+    communication_socket.close()
+    print(f"Connection with {address} ended!")
